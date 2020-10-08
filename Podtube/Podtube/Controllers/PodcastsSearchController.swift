72c739d785b50d34d047bca93e2fe1cc7dfe48b5
CoreyBurgin <corey.burgin22@gmail.com>
2020-10-08 10:19:46 -0400

    PodcastCell
    creating two new files to get the Podcast cel UI Started. Registering the PodcastCell.nib file with the PodcastCell.swift file so the dequeuing works correctly

--- Podtube/Podtube/Controllers/PodcastsSearchController.swift
+++ Podtube/Podtube/Controllers/PodcastsSearchController.swift
@@ -47,7 +47,10 @@ class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
     }
     
     fileprivate func setupTableView() {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+        let nib = UINib(nibName: "PodcastCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: cellId)
     }
     
     //MARK:- UITableView
@@ -57,14 +60,22 @@ class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! PodcastCell
         
         let podcast = self.podcasts[indexPath.row]
-        cell.textLabel?.text = "\(podcast.trackName ?? "")\n\(podcast.artistName ?? "")"
-        cell.textLabel?.numberOfLines = -1
-        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
+        cell.podcast = podcast
+        
+        
+//        let podcast = self.podcasts[indexPath.row]
+//        cell.textLabel?.text = "\(podcast.trackName ?? "")\n\(podcast.artistName ?? "")"
+//        cell.textLabel?.numberOfLines = -1
+//        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 132
     }
 }
 
---
+++ Podtube/Podtube/Views/PodcastCell.swift
@@ -0,0 +1,23 @@
+//
+//  PodcastCell.swift
+//  Podtube
+//
+//  Created by Corey Burgin on 10/8/20.
+//
+
+import UIKit
+
+class PodcastCell: UITableViewCell {
+    
+    @IBOutlet weak var podcastImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var episodeCountLabel: UILabel!
+    
+    var podcast: Podcast! {
+        didSet {
+            trackNameLabel.text = podcast.trackName
+            artistNameLabel.text = podcast.artistName
+        }
+    }
+}
